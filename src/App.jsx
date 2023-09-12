1b4b12d6bfe457fea79909eddf2335b4debdba49
Piotr <piotr_as@yahoo.com>
2023-09-12 11:03:57 +0200

    task 2 solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 
 import usersFromServer from './api/users';
@@ -6,11 +6,29 @@ import categoriesFromServer from './api/categories';
 import productsFromServer from './api/products';
 
 export const App = () => {
-  const products = productsFromServer.map((product) => {
-    const category = categoriesFromServer
-      .find(cat => cat.id === product.categoryId);
-    const user = usersFromServer
-      .find(usr => usr.id === category.ownerId);
+  const [selectedUser, setSelectedUser] = useState('All'); // Default selection is 'All'
+
+  // Filter products based on the selected user
+  const filteredProducts = productsFromServer.filter((product) => {
+    const category
+    = categoriesFromServer.find(cat => cat.id === product.categoryId);
+    const user = usersFromServer.find(usr => usr.id === category.ownerId);
+
+    return selectedUser === 'All' || user.name === selectedUser;
+  });
+
+  const uniqueUserNames = [...new Set(usersFromServer.map(user => user.name))];
+
+  uniqueUserNames.unshift('All');
+
+  const handleUserChange = (event) => {
+    setSelectedUser(event.target.value);
+  };
+
+  const products = filteredProducts.map((product) => {
+    const category
+    = categoriesFromServer.find(cat => cat.id === product.categoryId);
+    const user = usersFromServer.find(usr => usr.id === category.ownerId);
     const ownerTextColorClass
     = user.sex === 'm' ? 'has-text-link' : 'has-text-danger';
 
@@ -30,7 +48,10 @@ export const App = () => {
           {` `}
           {category.title}
         </td>
-        <td data-cy="ProductUser" className={ownerTextColorClass}>
+        <td
+          data-cy="ProductUser"
+          className={`${user.name === selectedUser ? 'is-active ' : ''}${ownerTextColorClass}`}
+        >
           {user.name}
         </td>
       </tr>
@@ -42,6 +63,32 @@ export const App = () => {
       <div className="container">
         <h1 className="title">Product Categories</h1>
         <div className="box table-container">
+          <div className="field">
+            <label htmlFor="ownerFilter" className="label">
+              Filter by Owner:
+            </label>
+            <div className="control">
+              <div className="select">
+                <select
+                  id="ownerFilter"
+                  value={selectedUser}
+                  onChange={handleUserChange}
+                  data-cy="OwnerFilter"
+                >
+                  {uniqueUserNames.map(userName => (
+                    <option
+                      key={userName}
+                      value={userName}
+                      className={userName === selectedUser ? 'is-active' : ''}
+                    >
+                      {userName}
+                    </option>
+                  ))}
+                </select>
+              </div>
+            </div>
+          </div>
+
           <p data-cy="NoMatchingMessage">
             No products matching selected criteria
           </p>
@@ -49,50 +96,6 @@ export const App = () => {
             data-cy="ProductTable"
             className="table is-striped is-narrow is-fullwidth"
           >
-            <thead>
-              <tr>
-                <th>
-                  <span className="is-flex is-flex-wrap-nowrap">
-                    ID
-                    <a href="#/">
-                      <span className="icon">
-                        <i data-cy="SortIcon" className="fas fa-sort" />
-                      </span>
-                    </a>
-                  </span>
-                </th>
-                <th>
-                  <span className="is-flex is-flex-wrap-nowrap">
-                    Product
-                    <a href="#/">
-                      <span className="icon">
-                        <i data-cy="SortIcon" className="fas fa-sort-down" />
-                      </span>
-                    </a>
-                  </span>
-                </th>
-                <th>
-                  <span className="is-flex is-flex-wrap-nowrap">
-                    Category
-                    <a href="#/">
-                      <span className="icon">
-                        <i data-cy="SortIcon" className="fas fa-sort-up" />
-                      </span>
-                    </a>
-                  </span>
-                </th>
-                <th>
-                  <span className="is-flex is-flex-wrap-nowrap">
-                    User
-                    <a href="#/">
-                      <span className="icon">
-                        <i data-cy="SortIcon" className="fas fa-sort" />
-                      </span>
-                    </a>
-                  </span>
-                </th>
-              </tr>
-            </thead>
             <tbody>{products}</tbody>
           </table>
         </div>
